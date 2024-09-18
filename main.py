f6eee8452214d78f38d5d856b37b5d2af395f9d6
Varun <panjwanivarun12@gmail.com>
2024-09-18 19:37:01 -0400

    Loaded the content from sql file into the table created.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QWidget, QLineEdit, QApplication, QLabel, QPushButton, QGridLayout, QMainWindow, \
-    QTableWidget
+    QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -18,15 +19,26 @@ class MainWindow(QMainWindow):
         help_button.addAction(help_content)
 
         # ADD TABLE
-        table = QTableWidget()
-        table.setColumnCount(4)
-        table.setHorizontalHeaderLabels(("ID", "NAME", "COURSE", "MOBILE"))
-        self.setCentralWidget(table)
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "NAME", "COURSE", "MOBILE"))
+        self.table.verticalHeader().setVisible(False) # to avoid default row number from displaying
+        self.setCentralWidget(self.table)
 
-
+    # add data
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+        connection.close()
 
 
 app = QApplication(sys.argv)
 std = MainWindow()
+std.load_data()
 std.show()
 sys.exit(app.exec())
